be0693e98a67fb9238e382f1ce67fdd42f046e87
DESKTOP-NAQBUOF\Tuan Anh <anhh12318@gmail.com>
2025-03-11 11:54:09 +0700

    (...)

--- bmt.fe/src/pages/detailMovie/DetailMovie.jsx
+++ bmt.fe/src/pages/detailMovie/DetailMovie.jsx
@@ -91,10 +91,18 @@ const DetailMovie = () => {
                   width={800}
                   centered
                 >
-                  <iframe width= "100%" height="400" src="https://www.youtube.com/embed/Yz96EBNwMGw?si=T7pSIpVvyNMmGdkD"
-                    title="YouTube video player" frameborder="0"
+                  <iframe
+                    width="100%"
+                    height="400"
+                    src={movie.trailerUrl}
+                    title="YouTube video player"
+                    frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                  ></iframe>
+
+
                 </Modal>
               </>
             )}
